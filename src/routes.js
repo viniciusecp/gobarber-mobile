4d1bc361706f32ce7d64ec8b7ce5469eb7c7eebf
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2020-02-05 22:06:39 -0300

    Implemented dashboard route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn/index';
 import SignUp from './pages/SignUp/index';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  })
-);
+import Dashboard from './pages/Dashboard';
+
+export default (signedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: signedIn ? 'App' : 'Sign',
+      }
+    )
+  );
